bac462722d6761f4105ba2ed8dc3ffbba74b03e8
brian d foy <brian.d.foy@gmail.com>
2022-01-08 23:07:28 -0500

    bump copyright year

--- lib/Business/ISBN13.pm
+++ lib/Business/ISBN13.pm
@@ -114,7 +114,7 @@ brian d foy C<< <bdfoy@cpan.org> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2001-2021, brian d foy <bdfoy@cpan.org>. All rights reserved.
+Copyright © 2001-2022, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
 This module is licensed under the Artistic License 2.0. See the LICENSE
 file in the distribution, or https://opensource.org/licenses/Artistic-2.0
