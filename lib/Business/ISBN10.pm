c0d18f930fc0c7584c2fc43d32e5709aab71f9b3
brian d foy <briandfoy@pobox.com>
2025-01-03 21:51:20 -0500

    update copyright year

--- lib/Business/ISBN10.pm
+++ lib/Business/ISBN10.pm
@@ -106,7 +106,7 @@ brian d foy C<< <briandfoy@pobox.com> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2001-2024, brian d foy <briandfoy@pobox.com>. All rights reserved.
+Copyright © 2001-2025, brian d foy <briandfoy@pobox.com>. All rights reserved.
 
 This module is licensed under the Artistic License 2.0. See the LICENSE
 file in the distribution, or https://opensource.org/licenses/Artistic-2.0
